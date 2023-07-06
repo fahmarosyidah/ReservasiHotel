a76e158c261315982d703c7d9042e431025971df
fahmarosyidah <fahmarosyi@gmail.com>
2023-07-06 18:05:38 +0700

    first commit

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -29,10 +29,17 @@ namespace ReservasiHotel
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form3";
+            this.Name = "Form3";
+            this.Text = "Data Tamu";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
